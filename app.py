1ecad3b4aa39eeee5becd0a73d1cc4634120d4eb
tolossamuel <tolossamuel1@gmail.com>
2024-07-15 11:29:26 +0300

    carRental

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def predict_city(query: str):
     return sorted_cities[:10]
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    uvicorn.run(app, host='0.0.0.0', port=8000)
 
 
 '''
